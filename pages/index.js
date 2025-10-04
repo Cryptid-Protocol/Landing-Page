6b23ffbb2a8096eb62ecaee742e35aa5a421681c
CryptidPro <kaeimlay@gmail.com>
2025-10-03 21:30:33 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,25 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import Features from '../components/Features'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+    <title>Cryptidm Protocol - The Visible Blockchain</title>
+  <meta name="description" content="Watch development happen in real-time. Earn crypto for building the future." />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect' href="https://fonts.gstatic.com" crossOrgin="true" />
+  <lik href="http://fonts.googlrapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet' />
+    </Head>
+
+  <Header />
+    <Hero />
+    <Features />
+    <Footer />
+    </>
+  )
+}
 
