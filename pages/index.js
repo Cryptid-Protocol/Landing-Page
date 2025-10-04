a6e239b1f7ed7ba95429c62436200b634f5c13b4
CryptidPro <kaeimlay@gmail.com>
2025-10-04 13:38:26 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Header from '../components/Header'
-import Hero from '../components/Hero'
+import HeroSection from '../components/HeroSection'
 import Features from '../components/Features'
 import Footer from '../components/Footer'
 
