6e34f5dd067e6c23050fdffdc7a8b15caec4c59a
CryptidPro <kaeimlay@gmail.com>
2025-10-04 12:50:14 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,8 @@ export default function Home() {
   <meta name="description" content="Watch development happen in real-time. Earn crypto for building the future." />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect' href="https://fonts.gstatic.com" crossOrgin="true" />
-  <lik href="http://fonts.googlrapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet' 
-    />
+  <lik href="http://fonts.googlrapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" 
+    rel="stylesheet'  />
     </Head>
 
   <Header />
